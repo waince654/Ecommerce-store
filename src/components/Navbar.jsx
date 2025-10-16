97f77a7ccd8a62d14587f324c9a75b4eb4afe814
waince654 <mfarooqjjj5@gmail.com>
2025-10-16 03:05:31 -0700

    add navbar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,6 +32,65 @@ const Navbar = () => {
             <FaCaretDown />
           </div>
         </div>
+        {/* menu section */}
+        <nav className="flex gap-7 items-center">
+          <ul className="md:flex gap-7 items-center text-xl font-semibold hidden">
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Home</li>
+            </NavLink>
+            <NavLink
+              to={"/products"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Products</li>
+            </NavLink>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>About</li>
+            </NavLink>
+            <NavLink
+              to={"/contact"}
+              className={({ isActive }) =>
+                `${
+                  isActive
+                    ? "border-b-3 transition-all border-red-500"
+                    : "text-black"
+                } cursor-pointer`
+              }
+            >
+              <li>Contact</li>
+            </NavLink>
+          </ul>
+          <Link to={"/cart"} className="relative">
+            <IoCartOutline className="h-7 w-7" />
+            <span className="bg-red-500 px-2 rounded-full absolute -top-3 -right-3 text-white">
+              0
+            </span>
+          </Link>
+        </nav>
       </div>
     </div>
   );
