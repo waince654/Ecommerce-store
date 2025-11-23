13c0889ad15eb994972ad83e16cbbb5e991c37f5
waince654 <mfarooqjjj5@gmail.com>
2025-11-23 01:07:06 -0800

    add mid banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect } from "react";
 import Carousel from "../components/Carousel";
+import MidBanner from "../components/MidBanner";
 const Home = () => {
   return (
     <div className="overflow-x-hidden">
-      <Carousel />     
+      <Carousel />  
+      <MidBanner/>   
     </div>
   );
 };
