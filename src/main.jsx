71d03c5dd5535d441453e2cc796d594ff9079806
waince654 <mfarooqjjj5@gmail.com>
2025-11-23 22:47:24 -0800

    add toastify on carted product

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
-import { DataProvider } from './context/DataContext.jsx'
+import { DataProvider } from "./context/DataContext.jsx";
+import { Bounce } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -14,10 +18,23 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <DataProvider>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <DataProvider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          transition={Bounce}
+        />
+      </ClerkProvider>
     </DataProvider>
   </StrictMode>
 );
